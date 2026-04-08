7e73c800b1afbec5b23ae3235ade2595c2999001
adrianolaudando1-coder <adriano.laudando1@gmail.com>
2026-04-08 21:44:31 +0200

    Create CardLogo.tsx

---
+++ components/CardLogo.tsx
@@ -0,0 +1,14 @@
+export default function CardLogo() {
+  return (
+    <img
+      src="/logo.png"
+      alt="Logo"
+      style={{
+        height: '40px',
+        width: 'auto',
+        alignSelf: 'center',
+        marginBottom: '6px',
+      }}
+    />
+  );
+}
